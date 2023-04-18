4382fac20614530c6a6d565dae30da9979c68136
KarlinaU <Karlina.ulme@svg.lv>
2023-04-18 14:47:53 +0300

    !

---
+++ projekts.official/sakumlapa.js
@@ -0,0 +1,14 @@
+var startnum = 0
+var convstart = 0
+var convend = 0
+
+function CalcNum(){
+    var selector = document.getElementById('mervieniba1');
+    var convstart = selector[selector.selectedIndex].value;
+    startnum = document.getElementById("skaitlis1").value
+    startnum*=convstart
+    var selector = document.getElementById('mervieniba2');
+    var convend = selector[selector.selectedIndex].value;
+    startnum*=convend
+    document.getElementById("Atbilde").value = startnum
+}
