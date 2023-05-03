4687d2b786add7a6c78655f802ccabfb932c4128
KarlinaU <Karlina.ulme@svg.lv>
2023-05-03 09:32:51 +0300

    .

---
+++ projekts.official/sakumlapa.js
@@ -0,0 +1,8 @@
+window.alert("Sveicināti!");
+alert("Jūs esat atraduši manu mājaslapu - mērvienību kalkulators");
+
+if (confirm("Press a button!")) {
+    txt = "You pressed OK!";
+  } else {
+    txt = "You pressed Cancel!";
+  }
